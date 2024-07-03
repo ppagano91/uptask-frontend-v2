61ff7ebd0bde079e0f3b3c70ee0eec60a4859189
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-03 12:34:46 -0300

    Configuración axios

---
+++ src/api/ProjectAPI.ts
@@ -0,0 +1,5 @@
+import { ProjectFormData } from "@/types/index";
+
+export async function createProject(data: ProjectFormData){
+    console.log(data)
+}
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ProjectForm from "@/components/projects/ProjectForm";
-import { ProjectFormData } from "types";
+import { ProjectFormData } from "@/types/index";
+import { createProject } from "@/api/ProjectAPI";
 
 const CreateProjectView = () => {
 
@@ -14,7 +15,7 @@ const CreateProjectView = () => {
   const {register, handleSubmit, formState:{errors}} = useForm({defaultValues: initialValues});
   
   const handleForm = (data: ProjectFormData) => {
-    console.log(data)
+    createProject(data);
   }
   
   return (

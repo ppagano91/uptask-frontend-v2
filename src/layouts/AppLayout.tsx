b10b80a3828003e0026ed973131574bc73722c61
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-03 09:39:33 -0300

    Componente NaveMenu

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 const AppLayout = () => {
   return (
     <>
@@ -8,6 +9,7 @@ const AppLayout = () => {
           <div className="w-64">
             <Logo/>
           </div>
+          <NavMenu />
         </div>
       </header>
       <section className="max-w-screen-2xl mx-auto mt-10 p-5">

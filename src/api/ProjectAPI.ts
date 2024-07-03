6e278ba0f2f7f39b32ac6c9496125bb9a6a88917
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-03 14:00:38 -0300

    Funcionalidad para crear Proyecto

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,11 @@
+import api from "@/lib/axios";
 import { ProjectFormData } from "@/types/index";
 
-export async function createProject(data: ProjectFormData){
-    console.log(data)
+export async function createProject(formData: ProjectFormData){
+    try {
+        const { data } = await api.post("/projects", formData);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 }

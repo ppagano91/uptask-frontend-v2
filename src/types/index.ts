69de758cf9020afa6e1c235d07b47e9a7437e342
Patricio Pagano <pagano.patricio@gmail.com>
2024-07-04 21:45:26 -0300

    Formulario para agregar Tarea

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,5 +20,21 @@ export const dashboardProjectSchema = z.object({
     )
 });
 
+
 export type Project = z.infer<typeof projectSchema>;
 export type ProjectFormData = Pick<Project, "clientName" | "projectName" | "description">;
+
+
+/** Tasks */
+export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
+
+export const taskSchema = z.object({
+    _id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    project: z.string(),
+    status: taskStatusSchema,
+});
+
+export type Task = z.infer<typeof taskSchema>;
+export type TaskFormData = Pick<Task, "name" | "description">;
